1f9ab4f20d5aefc99e431eaf9ead8f93177463a5
Bandy <32261585+Bandy0121@users.noreply.github.com>
2025-03-08 01:01:22 +0800

    .

--- HomeWork/LEVEL2/以太坊ethclient包使用/2.12 执行合约/main.go
+++ HomeWork/LEVEL2/以太坊ethclient包使用/2.12 执行合约/main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/crypto"
 	"log"
-	"math/big"
 	"os"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -20,10 +19,10 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	privateKey, err := crypto.HexToECDSA("38edd948f94c292109f1ab6a8315a6d1c2934ee0f514047d41cb7bb25087ff4c")
-	if err != nil {
-		log.Fatal(err)
-	}
+	//privateKey, err := crypto.HexToECDSA("38edd948f94c292109f1ab6a8315a6d1c2934ee0f514047d41cb7bb25087ff4c")
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
 
 	contractAddr := common.HexToAddress("0x1399E0Fa83fBc9d1f34E16fe8603CF1348aA943B")
 	// 从文件读取ABI内容也是可行的
@@ -55,16 +54,16 @@ func main() {
 	copy(value[:], []byte("demo_save_value11111"))
 
 	//初始化交易opt实例
-	opt, err := bind.NewKeyedTransactorWithChainID(privateKey, big.NewInt(11155111))
-	if err != nil {
-		log.Fatal(err)
-	}
-	// 调用合约方法
-	tx, err := contract.Transact(opt, "setItem", key, value)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("tx hash:", tx.Hash().Hex())
+	//opt, err := bind.NewKeyedTransactorWithChainID(privateKey, big.NewInt(11155111))
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//// 调用合约方法
+	//tx, err := contract.Transact(opt, "setItem", key, value)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//fmt.Println("tx hash:", tx.Hash().Hex())
 
 	//调用Items()
 	var results []interface{}
@@ -77,6 +76,20 @@ func main() {
 		if valueInContract, ok := results[0].([32]byte); ok {
 			fmt.Println("valueInContract：", valueInContract)
 			fmt.Println("is value saving in contract equals to origin value:", valueInContract == value)
+			fmt.Println("valueString1：", string(valueInContract[:])) //"valueString1： demo_save_value11111            "//未初始化的字节默认值 0 被强制转成字符串，导致多余内容显示
+			validLen := 0
+			for ; validLen < len(valueInContract); validLen++ {
+				if value[validLen] == 0 {
+					break
+				}
+			}
+			fmt.Println("valueString2：", string(value[:validLen])) //"valueString2： demo_save_value11111"
+			index := bytes.IndexByte(valueInContract[:], 0)        //bytes.IndexByte 函数，它能返回指定字节在字节切片中首次出现的索引，若没找到则返回 -1
+			if index == -1 {
+				fmt.Println("valueString3：", string(valueInContract[:]))
+			} else {
+				fmt.Println("valueString3：", string(valueInContract[:index]))
+			}
 			item := hexutil.Encode(valueInContract[:]) //把[32]byte的_item转成切片[]byte
 			fmt.Println("item：", item)
 		} else {
